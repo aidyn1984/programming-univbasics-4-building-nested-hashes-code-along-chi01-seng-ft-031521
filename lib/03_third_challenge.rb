19673302c7838b562235bf5dbe70db564c5ff67d
Aiden Seitz <csa.aiden@gmail.com>
2021-02-25 04:54:27 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -1,17 +1,17 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => { :name "Lord Montague", :age 53},
-      :matriarch => {:name "Lady Montague", :age 54
+      :patriarch => { name: "Lord Montague", age: 53},
+      :matriarch => { name: "Lady Montague", age: 54
         },
-      :hero => {:name "Romeo", :age 15 :s },
-      :hero_friends => [{name:}]
+      :hero => { name: "Romeo", age: 15, status: "alive" },
+      :hero_friends => []
    }, 
    :capulet => {
-      :patriarch => { :name "Lord Capulet", :age 50},
-      :matriarch => {:name "Lady Capulet", :age 51
+      :patriarch => { name: "Lord Capulet", age: 50},
+      :matriarch => { name: "Lady Capulet", age: 51
         },
-      :hero => {:name "Juliet", :age 15 },
+      :hero => {name: "Juliet", age: 15, status: "alive" },
       :heroine_friends => []
    }
   }
